0aabf518a30b02536a910f5b77f866c0093a9c4c
Rahul Singh <rahul2018me015abesit@gmail.com>
2023-01-16 20:34:19 +0530

    fifth commit

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -1,43 +1,122 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Form = () => {
-
   // this is a two input form useState hook method that use in the forms
   // one useState is used to store the written information and another is used two show that stored information
-    const[name,setName]= useState('');
-    const[showForm,setShowform]= useState('');
-    const[lastName,setLastName]= useState('');
-    const[showFormTwo,setShowFormTwo] = useState('');
+  // now new method is used to simplify the code
+  // const[name,setName]= useState('');
+  // const[showForm,setShowform]= useState('');
+  // const[lastName,setLastName]= useState('');
+  // const[showFormTwo,setShowFormTwo] = useState('');
 
-    
+  const [name, setName] = useState({
+    fName: "",
+    lName: "",
+    eMail: "",
+  });
 
-    const inputEvent = (event)=>{
-        console.log(event.target.value);
-        setName(event.target.value)
-    }
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    // setName(event.target.value)
+    // the below two console will provide you the exact information yhata user has enterd in the form
+    console.log(event.target.name);
+    console.log(event.target.placeholder);
+    // below two constants will be used to access the entered text by the user
+    const namee = event.target.name;
+    const value = event.target.value;
 
-    const submitForm =(e)=>{
-      e.preventDefault();
-      setShowform(name);
-      setShowFormTwo(lastName);
-    }
+    // demonstration of object destructuring
 
-    const inputEventTwo = (event)=>{
-      console.log(event.target.value);
-      setLastName(event.target.value);
-    }
+    // const {namee,value} = event.target;
 
+    // setname will set the value of the enterd by the user
+    setName((previousValue) => {
+      console.log(previousValue);
+      return {
+        // jo bhi previousvalue object mein name se match karte hai use value mein add kardo
+        ...previousValue,
+        [namee] : value,
+      }
+
+
+
+      // it is commit sho thata we can use spread operator
+      // if (namee === "fName") {
+      //   return {
+      //     fName: value,
+      //     lName: previousValue.lName,
+      //     eMail: previousValue.eMail,
+      //   };
+      // } else if (namee === "lName") {
+      //   return {
+      //     fName: previousValue.fName,
+      //     lName: value,
+      //     eMail: previousValue.eMail,
+      //   };
+      // } else if (namee === "eMail") {
+      //   return {
+      //     fName: previousValue.fName,
+      //     lName: previousValue.lName,
+      //     eMail: value,
+      //   };
+      // }
+    });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    alert("form Submitted😄");
+    // simplifying the form
+    // setShowform(name);
+    // setShowFormTwo(lastName);
+  };
+
+  // it is done for the reduction if the code
+  // const inputEventTwo = (event)=>{
+  //   console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // }
 
   return (
     <>
-    <form onClick={submitForm}>
-    <h1>Hello This is {showForm} {showFormTwo}!</h1>
-    <input type="text" value={name} placeholder='Enter your namr here' onChange={inputEvent} />
-    <input type="text" value={lastName} placeholder='Enter your namr here' onChange={inputEventTwo} />
-    <button type='submit'>Click Me</button>
-    </form>
-    </>
-  )
-}
+      <form onClick={submitForm}>
+        {/* this is done for reduction of the code */}
+        {/* <h1>Hello This is {showForm} {showFormTwo}!</h1> */}
+        <h1>
+          Hello {name.fName} {name.lName} {name.eMail}
+        </h1>
+        <input
+          type="text"
+          //  value={name}
+          placeholder="Enter your name here"
+          name="fName"
+          onChange={inputEvent}
+          value={name.fName}
+        />
+        <input
+          type="email"
+          //  value={lastName}
+          placeholder="Enter your lastname here"
+          //  onChange={inputEventTwo}
+          onChange={inputEvent}
+          value={name.lName}
+          name="lName"
+        />
+        <input
+          type="text"
+          //  value={lastName}
+          placeholder="Enter your email here"
+          //  onChange={inputEventTwo}
+          onChange={inputEvent}
+          value={name.eMail}
+          name="eMail"
+          autoComplete="off"
+        />
 
-export default Form
+        <button type="submit">Click Me</button>
+      </form>
+    </>
+  );
+};
+
+export default Form;
